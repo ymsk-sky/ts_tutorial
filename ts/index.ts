c19824d6a7483d139dd747abde25e99b48fd25f3
ymsk.sky <ymsk.sky.95@gmail.com>
2020-04-06 22:11:35 +0900

    use module and UUID library

--- ts/index.ts
+++ ts/index.ts
@@ -1,17 +1,18 @@
-class User {
-  public age: number;
-  public familyName: string;
-  public givenName: string;
-  constructor (familyName: string, givenName: string, age: number) {
-    this.age = age;
-    this.familyName = familyName;
-    this.givenName = givenName;
-  }
-}
+import { v4 as uuidv4 } from 'uuid';
+
+import { User } from "./User"
 
 const user = new User('YMSK', 'SKY', 18);
 
 const contentsElem = document.getElementById('contents');
 if(!!contentsElem) {
   contentsElem.innerText = `${user.familyName} ${user.givenName}`;
+}
+
+// HTML要素を取得 キャストすることで入力補完が効く
+const saibanButton = document.getElementById('saiban') as HTMLButtonElement;
+const uuidSpan = document.getElementById('uuid') as HTMLSpanElement;
+// ボタンのクリックイベントを追加
+saibanButton.onclick = (e) => {
+  uuidSpan.innerText = uuidv4();
 }
